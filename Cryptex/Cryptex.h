b3b1a68f7049737142cbe3d1b3468bc70caff3bc
Sathya <trsathya@gmail.com>
2018-04-23 01:00:19 +0530

    Add workspace and framework project

---
+++ Cryptex/Cryptex.h
@@ -0,0 +1,19 @@
+//
+//  Cryptex.h
+//  Cryptex
+//
+//  Created by Sathya on 4/22/18.
+//  Copyright © 2018 Sathya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cryptex.
+FOUNDATION_EXPORT double CryptexVersionNumber;
+
+//! Project version string for Cryptex.
+FOUNDATION_EXPORT const unsigned char CryptexVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cryptex/PublicHeader.h>
+
+
