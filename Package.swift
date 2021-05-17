eaf2b9094352c2ef1016a83b618618c189dc9389
Alley Pereira <noyalle@ualberta.ca>
2021-05-17 20:20:37 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "0.8.1")
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.3.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
